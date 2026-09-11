8d967a1c8fc1ae95fbc6eb9915d940deb53ad1ae
Alicja Magdalena <alicia.madeleine.k@gmail.com>
2026-09-11 15:49:13 +0200

    kod okresla czy wystepuje podstawa chmury

--- main.R
+++ main.R
@@ -1,4 +1,24 @@
 install.packages("climate")
 library(climate)
 
-?climate
+dzis <- Sys.Date()
+
+legionowo <- sounding_wyoming(
+  wmo_id = 12374,
+  yy = as.numeric(format(dzis, "%Y")),
+  mm = as.numeric(format(dzis, "%m")),
+  dd = as.numeric(format(dzis, "%d")),
+  hh = 12
+)
+
+profil   <- legionowo[[1]]
+
+warunek_powstania_chmury <- which(profil$TEMP == profil$DWPT)
+
+if (length(warunek_powstania_chmury) > 0) {
+  podstawa_chmury <- profil$HGHT[warunek_powstania_chmury[1]]
+} else {
+  podstawa_chmury <- NA
+}
+
+podstawa_chmury
