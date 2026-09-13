22c24ea8597d4f294b49343f9b97beb8725d53d0
Alunia <alicia.madeleine.k@gmail.com>
2026-09-13 12:37:36 +0200

    Update main.R
    lcl

--- main.R
+++ main.R
@@ -22,3 +22,17 @@ if (length(warunek_powstania_chmury) > 0) {
 }
 
 podstawa_chmury
+
+for (i in 1:nrow(profil)) {
+  wysokosc <- profil$HGHT[i]
+  if (is.na(wysokosc)) next
+  delta_H <- wysokosc - H0
+  T_parcel <- T0 - (0.0098 * delta_H)
+  Td_parcel <- Td0 - (0.0018 * delta_H)
+  if (!is.na(T_parcel) && !is.na(Td_parcel) && T_parcel <= Td_parcel) {
+    LCL_hght <- wysokosc
+    break
+  }
+}
+
+LCL_hght
